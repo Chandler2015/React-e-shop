fb30677e4b30330fce58cbfb6d429319f0d1d452
YuhangXiong <yxiong4@ualberta.ca>
2019-09-21 21:26:10 -0600

    add collection-item, collection-preview and styles and shopping data

--- src/App.js
+++ src/App.js
@@ -3,19 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import {Switch,Route} from 'react-router-dom';
+import Shoppage from './pages/shop/shop.component';
 
-const Hatspage = ()=> (
-  <div>
-    <h1>Hats page</h1>
-  </div>
-);
+
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={Homepage}/>
-        <Route exact path='/hats' component={Hatspage}/>
+        <Route exact path='/shop' component={Shoppage}/>
       </Switch>
     </div>
   );
