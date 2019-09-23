08dfe0dcf5c54ce33c4195d2a6b5a70547e2844b
YuhangXiong <yxiong4@ualberta.ca>
2019-09-23 02:01:23 -0600

    add cart reducer and user reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -10,7 +10,7 @@ import './header.styles.scss';
 import {auth} from '../../firebase/firebase.utils';
 
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser,hidden}) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
@@ -31,12 +31,14 @@ const Header = ({currentUser}) => (
       }
       <CartIcon/>
     </div>
-    <CartDropdown/>
+    {hidden?null:
+    <CartDropdown/>}
   </div>
 );
 
-const mapStateToProps = state =>({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({user:{currentUser},cart:{hidden}}) =>({
+  currentUser,
+  hidden
 })
 
 
