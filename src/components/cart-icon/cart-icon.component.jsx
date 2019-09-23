a8a5bd694b5f9203c20485f84bc7ccc58f33934d
YuhangXiong <yxiong4@ualberta.ca>
2019-09-23 12:53:26 -0600

    updating application with reselect replace cart selectors with memorized selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -8,6 +8,8 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
+import {selectCartItemsCount} from '../../redux/cart/cart.selectors';
+
 const CartIcon = ({ toggleCartHidden,itemCount }) => (
   <div className='cart-icon' onClick={toggleCartHidden}>
     <ShoppingIcon className='shopping-icon' />
