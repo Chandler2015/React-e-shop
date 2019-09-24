1d0e4d78761708f10cddcd5825f0b71a34067794
YuhangXiong <yxiong4@ualberta.ca>
2019-09-24 14:56:19 -0600

    updating title and icon

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,7 +4,7 @@ import {connect} from 'react-redux';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { ReactComponent as Logo } from '../../assets/store.svg';
 
 import './header.styles.scss';
 import {auth} from '../../firebase/firebase.utils';
