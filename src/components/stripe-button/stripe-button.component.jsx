7a48670ecb2032b82e151f5ec4b2748dbc809050
YuhangXiong <yxiong4@ualberta.ca>
2019-09-24 14:35:13 -0600

    add stripe payment button and funtion to checkout page

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton=({price})=>{
+    const priceForStripe = price*100;
+    const publishableKey = 'pk_test_HHlq5pcmnwAPUb2DNNMHE3Ld00RttrdVZV';
+
+    const onToken = token =>{
+        console.log(token);
+        alert('Payment Successful');
+    }
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='e-shop ltd.'
+            billingAddress
+            shippingAddress
+            image= 'https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount = {priceForStripe}
+            panelLabel= 'Pay Now'
+            token={onToken}
+            stripeKey = {publishableKey}
+        />
+    );
+};
+
+export default StripeCheckoutButton;
+
+
+
