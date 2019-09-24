843277c630af192f816c139b154083a468da13a5
YuhangXiong <yxiong4@ualberta.ca>
2019-09-23 23:17:18 -0600

    add collection-overview component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,19 +1,14 @@
 import React from "react";
 
-import CollectionPreview from '../../components/preview-collection/preview-collection.component';
-import {connect} from 'react-redux';
-import {createStructuredSelector} from 'reselect';
-import {selectShopItems} from '../../redux/shop/shop.selectors';
+import CollectionOverview from '../../components/collections-overview/collections-overview.component';
+
 
 const Shoppage = ({collections})=>(
 
     <div className='shop-page'>
-        {
-            collections.map(({id,...otherCollectionProps})=>(
-            <CollectionPreview key={id} {...otherCollectionProps}/>
-            ))
-        }
+       <CollectionOverview/>
     </div>
     ); 
+
 
 export default Shoppage;
